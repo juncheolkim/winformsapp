f3746595c0c06e934e36af4e7fcceb953421db00
juncheolkim <kimjoon889@gmail.com>
2024-03-20 02:49:20 +0900

    DI를 위한 IDatabase 작성
    의존성 주입을 하기위해 IDatabase 작성

---
+++ Interface/IDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsAppMaster.Models;
+
+namespace WinFormsAppMaster.Interface
+{
+    public interface IDatabase<T>
+    {
+        // 테이블에서 전체 데이터 조회
+        List<T> Get();
+
+        // 테이블에서 특정 데이터 조회
+        T GetDetail(int? id);
+
+        // 테이블에 새로운 행 삽입
+        void Create(T entity);
+
+        // 테이블에 기존 행 업데이트
+        void Update(T entity);
+
+        // 테이블에 기존 행 삭제
+        void Delete(int? id);
+    }
+}
